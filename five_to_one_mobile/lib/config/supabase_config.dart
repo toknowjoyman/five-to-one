112f96f1c15fa302c974f3e15b46b22537bf6e19
tkjm <viditjoy@gmail.com>
2025-11-15 22:11:45 -0800

    Update supabase_config.dart

--- five_to_one_mobile/lib/config/supabase_config.dart
+++ five_to_one_mobile/lib/config/supabase_config.dart
@@ -4,8 +4,8 @@
 
 class SupabaseConfig {
   // Your Supabase project URL
-  static const String supabaseUrl = 'https://YOUR_PROJECT.supabase.co';
+  static const String supabaseUrl = 'https://moynkekavontkrmuqbgg.supabase.co';
 
   // Your Supabase anon/public key
-  static const String supabaseAnonKey = 'YOUR_ANON_KEY_HERE';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1veW5rZWthdm9udGtybXVxYmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyNDc5MDksImV4cCI6MjA3ODgyMzkwOX0.gClIqgYG4fr-tBC9kjagFmTCK57r2hDY0rjndOv_Nx8';
 }
